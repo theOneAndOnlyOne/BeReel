4e45cb20b543c19e558739cddf7ea8cce871c430
Kevin Samson <kevinsam2003@gmail.com>
2023-12-28 02:06:31 +0400

    one click recap button

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import requests
 from flask import Flask, render_template, request, jsonify
 from combineImages import create_images
 from generateSlideshow import buildSlideshow
+from recap import butidRecap
 from datetime import datetime
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="templates")
+
 
 # Acquire Phone Number from User
 def send_code(phone):
     print("> Entered phone number is ", phone)
     # First Post to send out OTP session and code
     url_send_code = "https://berealapi.fly.dev/login/send-code"
-    
+
     # IMPORTANT: Format must be +##########
     payload = {"phone": phone}
 
@@ -35,19 +37,17 @@ def send_code(phone):
 
     return otp_session
 
+
 # Verify Session using otp_session code and user entered otp_code recieved from phone notification
 def verify(otp_session, otp_code):
-    #print("please enter OTP code")
-    #otp_code = input()
+    # print("please enter OTP code")
+    # otp_code = input()
     print("> OTP: ", otp_code)
 
     # Second POST request to verify base don user input
     url_verify = "https://berealapi.fly.dev/login/verify"
 
-    payload_verify = {
-        "code": otp_code,
-        "otpSession": otp_session
-    }
+    payload_verify = {"code": otp_code, "otpSession": otp_session}
 
     print("-- Sending Verify Request --")
     response_verify = requests.post(url_verify, json=payload_verify)
@@ -65,27 +65,28 @@ def verify(otp_session, otp_code):
             print("No 'tokenObj' found in the response.")
             exit()
     else:
-        print("> Verification request failed with status code:", response_verify.status_code)
+        print(
+            "> Verification request failed with status code:",
+            response_verify.status_code,
+        )
         print(response_verify.json())
         exit()
 
     return tokenObj
 
 
-#Fetch user memories. Skip to this stage if we already acquired reusable token
+# Fetch user memories. Skip to this stage if we already acquired reusable token
 def get_memories(tokenObj, start_date_range, end_date_range):
     url_mem_feed = "https://berealapi.fly.dev/friends/mem-feed"
-    headers = {
-        "token": tokenObj
-    }
+    headers = {"token": tokenObj}
 
     # Create a folder named 'primary' if it doesn't exist
-    folder_name = 'primary'
+    folder_name = "primary"
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
     # Create a folder named 'secondary' if it doesn't exist
-    secondary_folder_name = 'secondary'
+    secondary_folder_name = "secondary"
     if not os.path.exists(secondary_folder_name):
         os.makedirs(secondary_folder_name)
 
@@ -98,32 +99,31 @@ def get_memories(tokenObj, start_date_range, end_date_range):
         # Process the response from mem-feed endpoint
         print("Response:", response_mem_feed.json())
         print("we did it yay")
-        response_data = response_mem_feed.json().get('data', {})
-        data_array = response_data.get('data', [])
+        response_data = response_mem_feed.json().get("data", {})
+        data_array = response_data.get("data", [])
 
     else:
         print("GET request failed with status code:", response_mem_feed.status_code)
 
-
     start_date_str = str(start_date_range)
     end_date_str = str(end_date_range)
     # Convert the input strings to datetime objects
-    start_date_object = datetime.strptime(start_date_str, '%Y-%m-%d')
-    end_date_object = datetime.strptime(end_date_str, '%Y-%m-%d')
+    start_date_object = datetime.strptime(start_date_str, "%Y-%m-%d")
+    end_date_object = datetime.strptime(end_date_str, "%Y-%m-%d")
 
     # Iterate through the 'data' array and download images
     for item in data_array:
-        image_url = item['primary'].get('url', '')
-        secondary_image_url = item['secondary'].get('url', '')
-        date = item['memoryDay']
-        date_object = datetime.strptime(date, '%Y-%m-%d')
+        image_url = item["primary"].get("url", "")
+        secondary_image_url = item["secondary"].get("url", "")
+        date = item["memoryDay"]
+        date_object = datetime.strptime(date, "%Y-%m-%d")
 
         if image_url and start_date_object <= date_object <= end_date_object:
             # Extracting the image name from the URL
-            image_name = date + "_" + image_url.split('/')[-1]
+            image_name = date + "_" + image_url.split("/")[-1]
             # Downloading the image
             image_path = os.path.join(folder_name, image_name)
-            with open(image_path, 'wb') as img_file:
+            with open(image_path, "wb") as img_file:
                 img_response = requests.get(image_url)
                 if img_response.status_code == 200:
                     img_file.write(img_response.content)
@@ -132,10 +132,10 @@ def get_memories(tokenObj, start_date_range, end_date_range):
                     print(f"Failed to download {image_name}")
         if secondary_image_url and start_date_object <= date_object <= end_date_object:
             # Extracting the image name from the URL
-            image_name = date + "_" + secondary_image_url.split('/')[-1]
+            image_name = date + "_" + secondary_image_url.split("/")[-1]
             # Downloading the image
             image_path = os.path.join(secondary_folder_name, image_name)
-            with open(image_path, 'wb') as img_file:
+            with open(image_path, "wb") as img_file:
                 img_response = requests.get(secondary_image_url)
                 if img_response.status_code == 200:
                     img_file.write(img_response.content)
@@ -144,57 +144,65 @@ def get_memories(tokenObj, start_date_range, end_date_range):
                     print(f"Failed to download {image_name}")
 
     return "complete"
+
+
 # All images referenced in the 'primary' URLs should now be saved in the 'primary' folder
 # 'secondary' URLS saved in 'secondary', etc.
 
 
-#-------------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------------
 # Flask App Routing
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        phone_number = request.form['phone_number']
+    if request.method == "POST":
+        phone_number = request.form["phone_number"]
         otp_session = send_code(phone_number)
 
-        if otp_session != 'n/a':
-            return render_template('verify.html', otp_session=otp_session)
+        if otp_session != "n/a":
+            return render_template("verify.html", otp_session=otp_session)
 
-        return render_template('index.html', message='Invalid phone number. Check formatting and Please try again.')
+        return render_template(
+            "index.html",
+            message="Invalid phone number. Check formatting and Please try again.",
+        )
 
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/verify', methods=['POST'])
+
+@app.route("/verify", methods=["POST"])
 def verify_code():
-    if request.method == 'POST':
-        user_code = request.form['verification_code']
-        otp_session = request.form['otp_session']
+    if request.method == "POST":
+        user_code = request.form["verification_code"]
+        otp_session = request.form["otp_session"]
         print("> verify_code otp_session: ", otp_session)
         tokenObj = verify(otp_session, user_code)
 
-        if tokenObj != 'n/a':
-            return render_template('process.html', tokenObj=tokenObj)
+        if tokenObj != "n/a":
+            return render_template("process.html", tokenObj=tokenObj)
 
         else:
-            return render_template('failure.html')
-        #return render_template('verify.html', tokenObj='n/a', message='Invalid verification code. Please try again.')
+            return render_template("failure.html")
+        # return render_template('verify.html', tokenObj='n/a', message='Invalid verification code. Please try again.')
 
-    return render_template('verify.html')
+    return render_template("verify.html")
 
-@app.route('/process', methods=['POST'])
+
+@app.route("/process", methods=["POST"])
 def process_data():
-    if request.method == 'POST':
-        start_date_range = request.form['start_date_range']
-        end_date_range = request.form['end_date_range']
-        wav_file = request.files['wav_file']
-        tokenObj = request.form['tokenObj']
-        mode = request.form.get('mode')
+    if request.method == "POST":
+        start_date_range = request.form["start_date_range"]
+        end_date_range = request.form["end_date_range"]
+        wav_file = request.files["wav_file"]
+        tokenObj = request.form["tokenObj"]
+        mode = request.form.get("mode")
 
         print("> HTML Form Elements: ")
-        print("start_date_range ",str(start_date_range))
-        print("end_date_range ",str(end_date_range))
-        print("wav_file ",str(wav_file))
-        print("mode",str(mode))
+        print("start_date_range ", str(start_date_range))
+        print("end_date_range ", str(end_date_range))
+        print("wav_file ", str(wav_file))
+        print("mode", str(mode))
         # Call get_memories function
 
         print("> donwloading music file locally: ")
@@ -213,21 +221,42 @@ def process_data():
         print("> downloading images locally")
         result = get_memories(tokenObj, start_date_range, end_date_range)
 
-        if result != 'n/a':
+        if result != "n/a":
             # Execute the Python functions
-            create_images() # process images and apply effects
+            create_images()  # process images and apply effects
             # do something with current page here
-            buildSlideshow(mode) # assemble files and load audio
+            buildSlideshow(mode)  # assemble files and load audio
             # do something with current page here
-            return render_template('preview.html')
+            return render_template("preview.html")
         else:
-            return render_template('failure.html')
-        
+            return render_template("failure.html")
 
-    return render_template('process.html')
+    return render_template("process.html")
 
-#@app.route('/run-python-functions', methods=['POST'])
-#def run_python_functions():
+
+@app.route("/recap", methods=["POST"])
+def recap():
+    if request.method == "POST":
+        start_date_range = "2023-01-01"
+        end_date_range = "2023-12-31"
+        tokenObj = request.form["tokenObj"]
+        print("> downloading images locally")
+        result = get_memories(tokenObj, start_date_range, end_date_range)
+        if result != "n/a":
+            # Execute the Python functions
+            create_images()  # process images and apply effects
+            # do something with current page here
+            butidRecap()  # assemble files and load audio
+            # do something with current page here
+            return render_template("preview.html")
+        else:
+            return render_template("failure.html")
+
+    return render_template("process.html")
+
+
+# @app.route('/run-python-functions', methods=['POST'])
+# def run_python_functions():
 #    try:
 #        # Execute the Python functions
 #        create_images() # process images and apply effects
@@ -239,31 +268,37 @@ def process_data():
 #        # Return a JSON response indicating failure
 #        return render_template('failure.html', message=str(e)) # Failure! redirect to failure page
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
-@app.route('/privacy')
+
+@app.route("/privacy")
 def privacy():
-    return render_template('privacy.html')
+    return render_template("privacy.html")
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template("contact.html")
 
-@app.route('/preview')
+
+@app.route("/preview")
 def preview():
-    return render_template('preview.html')
+    return render_template("preview.html")
 
-@app.route('/failure')
+
+@app.route("/failure")
 def failure():
-    return render_template('failure.html')
+    return render_template("failure.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
 
-#otp_session = send_code()
-#tokenObj = verify(otp_session)
-#get_memories(tokenObj)
-#create_images()
-#buildSlideshow()
+# otp_session = send_code()
+# tokenObj = verify(otp_session)
+# get_memories(tokenObj)
+# create_images()
+# buildSlideshow()
---
+++ recap.py
@@ -0,0 +1,145 @@
+from moviepy.editor import *
+import numpy as np
+import os
+import cv2
+from PIL import Image, ImageDraw, ImageFont
+
+
+def bezier_curve(t, p0, p1, p2, p3):
+    return (
+        (1 - t) ** 3 * p0
+        + 3 * (1 - t) ** 2 * t * p1
+        + 3 * (1 - t) * t**2 * p2
+        + t**3 * p3
+    )
+
+
+def add_text_to_frame(
+    frame,
+    text,
+    font_size,
+    frame_width,
+    frame_height,
+    font_path=os.getcwd()
+    + os.path.sep
+    + "static"
+    + os.path.sep
+    + "fonts"
+    + os.path.sep
+    + "Inter-SemiBold.ttf",
+    position=(0, 0),
+    text_color=(0, 0, 255),
+):
+    # Convert OpenCV frame to Pillow Image
+    img_pil = Image.fromarray(cv2.cvtColor(np.uint8(frame), cv2.COLOR_BGR2RGB))
+
+    # Create a draw object
+    draw = ImageDraw.Draw(img_pil)
+
+    # Specify font and size
+    font = ImageFont.truetype(font_path, font_size)
+
+    # Get text size
+    text_size = draw.textbbox((0, 0), text, font=font)
+
+    # Calculate the center of the frame with respect to text size
+    center_x = int((frame_width - text_size[2]) / 2) + position[0]
+    center_y = int((frame_height - text_size[3]) / 2) + position[1]
+
+    # Add text to the image
+    draw.text((center_x, center_y), text, font=font, fill=text_color)
+
+    # Convert Pillow Image back to OpenCV frame
+    frame_with_text = cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR)
+
+    return frame_with_text
+
+
+def generate_video(image_folder, output_path, frames_per_second, t_duration):
+    images = sorted(
+        [
+            os.path.join(image_folder, img)
+            for img in os.listdir(image_folder)
+            if img.endswith(".webp")
+        ]
+    )
+
+    # Generate the duration for each image based on bezier curve
+    durations = []
+    for i in range(len(images)):
+        t = i / len(images)
+        durations.append(bezier_curve(t, 2.0, 0.4, 0.3, 0.9))
+    total_duration = np.sum(durations)
+    normalized_durations = [
+        duration * (t_duration / total_duration) for duration in durations
+    ]
+    print("Generating part1")
+    part1 = ImageSequenceClip(images, durations=[2 / len(images)] * len(images))
+    part1.write_videofile("part1.mp4", fps=frames_per_second)
+
+    cap = cv2.VideoCapture("part1.mp4")
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    out = cv2.VideoWriter(
+        "output.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height)
+    )
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == True:
+            frame = add_text_to_frame(
+                frame,
+                "RECAP 2023",
+                200,
+                width,
+                height,
+                text_color=(255, 255, 255),
+            )
+            out.write(frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            break
+
+    # frame with red background
+    blank = np.zeros((height, width, 3), np.uint8)
+    blank = add_text_to_frame(
+        blank,
+        "RECAP 2023",
+        200,
+        width,
+        height,
+        text_color=(255, 255, 255),
+    )
+
+    for _ in range(75):
+        out.write(blank)
+
+    cap.release()
+    out.release()
+    print("Generating part2")
+
+    part2 = ImageSequenceClip(images, durations=normalized_durations)
+
+    # combine part1 and part2
+    clips = [VideoFileClip("output.mp4"), part2]
+    final_clip = concatenate_videoclips(clips, method="chain")
+    final_clip.write_videofile(
+        output_path, fps=frames_per_second, threads=6, codec="libx264"
+    )
+    # delete part1
+    os.remove("part1.mp4")
+    # delete output.mp4
+    os.remove("output.mp4")
+
+
+def butidRecap():
+    image_folder = os.path.join(os.getcwd(), "combined")
+    output_path = (
+        os.getcwd() + os.path.sep + "static" + os.path.sep + "slideshow_test.mp4"
+    )
+    frames_per_second = 30
+    total_duration = 30
+
+    generate_video(image_folder, output_path, frames_per_second, total_duration)
